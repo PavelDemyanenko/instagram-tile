1fcc938bb1edf8c4939f122561ea342561bb2e53
xeLL <conscious.suddens@gmail.com>
2018-06-15 22:44:17 +0300

    Add webpack for tile frontend

---
+++ tiles/instagram-tile/webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract(
+          {
+            fallback: 'style-loader',
+            use: ['css-loader', 'sass-loader']
+          })
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin({filename: 'style.css'}),
+    new HtmlWebpackPlugin({
+      inject: false,
+      hash: true,
+      template: './src/index.html',
+      filename: 'index.html'
+    })
+  ],
+  resolve: {
+    extensions: ['*', '.js']
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devServer: {
+    contentBase: './dist'
+  }
+};
